e8c7d4ab04982b523cba20c09027fd3ebbdc8515
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-23 17:08:48 -0300

    Requisito 17

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import RecipesList from '../components/RecipesList';
 
 function Drinks() {
   return (
     <div>
       <Header title="Bebidas" />
+      <RecipesList />
     </div>
   );
 }
