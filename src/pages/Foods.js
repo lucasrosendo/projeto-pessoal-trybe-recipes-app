462858b09e2223c4cb791bd4c4563239f7fa4462
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-23 19:06:52 -0300

    Requisitos 25 e 26

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Header from '../components/Header';
 import RecipesList from '../components/RecipesList';
 import Footer from '../components/Footer';
+import RecipeContext from '../context/RecipeContext';
 
 function Foods() {
+  const { directRequestFood, isDrinkOrMealLoading } = useContext(RecipeContext);
+
+  // useEffect com comportamento de ComponentDidMount
+  useEffect(() => {
+    directRequestFood();
+  }, []);
+
   return (
     <div>
       <Header title="Comidas" />
-      <RecipesList />
+      { !isDrinkOrMealLoading ? <RecipesList /> : <p>Carregando</p> }
       <Footer />
     </div>
   );
