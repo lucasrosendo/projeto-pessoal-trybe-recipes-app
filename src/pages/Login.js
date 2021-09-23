51fbd3bd9fc349b2d9d01c3e1f6a37e49da0086a
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-23 10:06:24 -0300

    Requisito 6, 7 e 8

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,10 @@
+// Importação do PropType com desconstrução das funções
+import { shape, func } from 'prop-types';
+// Importação do hook useState
 import React, { useState } from 'react';
 
-function Login() {
+// Componente funcional Local, desconstruindo o history para buscar rota para push
+function Login({ history }) {
   // Criação do getter e setter do Email, onde 'email' é o state que recebe o email digitado e o setEmail é responsável por alterar esse state, e o state inicial dele é ''.
   const [email, setEmail] = useState('');
   // Criação do getter e setter do Password, onde 'password' é o state que recebe o password digitado e o setPassword é responsável por alterar esse state, e o state inicial dele é ''.
@@ -12,6 +16,14 @@ function Login() {
     if (password.length <= MIN_PASSWORD_LENGTH) return false;
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) return false;
     return true;
+  };
+
+  // Função criada, requisito 6, com a finalidade de registrar no localStorage 2 tokens com valores de 1
+  const saveToken = () => {
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/comidas');
   };
 
   return (
@@ -44,11 +56,18 @@ function Login() {
         type="button"
         data-testid="login-submit-btn"
         disabled={ !checkValidity() }
+        onClick={ saveToken }
       >
         Entrar
       </button>
     </form>
   );
 }
+
+Login.propTypes = {
+  history: shape({
+    push: func,
+  }).isRequired,
+};
 
 export default Login;
