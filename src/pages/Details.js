5f120908e803419de216132e9c8425cd27670c27
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-24 09:43:29 -0300

    Requisitos 16 e 17

---
+++ src/pages/Details.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import RecipeContext from '../context/RecipeContext';
+
+function Detalhes() {
+  // Importa do Context o state mealsOrDrinks que é um array generico que usamos para trazer as informações da API e salvar nele
+  const { mealsOrDrinks } = useContext(RecipeContext);
+  const history = useHistory();
+
+  const renderDrink = () => (
+    // Pega o título do primeiro Drink do array
+    <h1>{ mealsOrDrinks[0].strDrink }</h1>
+  );
+
+  const renderFood = () => (
+    // Pega o título da primeira Comida do array
+    <h1>{ mealsOrDrinks[0].strMeal }</h1>
+  );
+
+  const render = () => {
+    // Descobre se no link há informação de '/comidas' ou '/bebidas' para gerar a renderização
+    const value = history.location.pathname;
+    if (value.includes('comidas')) {
+      return renderFood();
+    }
+    if (value.includes('bebidas')) {
+      return renderDrink();
+    }
+  };
+
+  return (
+    render()
+  );
+}
+
+export default Detalhes;
