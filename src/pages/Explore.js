0f914e07fd2dbb0f7be0c3334f11958a8be96e43
Thiago <thiagoc.carboneri@gmail.com>
2021-09-27 16:33:54 -0300

    resoluçao dos req 67 ao 71

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,28 @@ function Explore() {
   return (
     <div>
       <Header title="Explorar" search={ false } />
+      <div
+        className="btn-container"
+      >
+        <Link to="/explorar/comidas">
+          { /* link que leva para a rota /explorar/comidas */ }
+          <button
+            type="button"
+            data-testid="explore-food"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/explorar/bebidas">
+          { /* link que leva para a rota /explorar/bebidas */ }
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/FoodExplore.js
+++ src/pages/FoodExplore.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function FoodExplore() {
   return (
     <div>
       <Header title="Explorar Comidas" search={ false } />
+      <div
+        className="explore-container"
+      >
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }
