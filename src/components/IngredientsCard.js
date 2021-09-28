0111cb3efa34555fc73a615d2f635f10e71b305f
Thiago <thiagoc.carboneri@gmail.com>
2021-09-28 10:54:29 -0300

    requisito 75

---
+++ src/components/IngredientsCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function IngredientsCard({ ingredient, index }) { // componente montado para renderizar os ingredientes tanto de bebidas quanto de comidas
+  return (
+    <div
+      className="ingredients-container"
+      data-testid={ `${index}-ingredient-card` }
+    >
+      <img
+        data-testid={ `${index}-card-img` }
+        src={ `https://www.themealdb.com/images/ingredients/${ingredient}-small.png` }
+        alt={ ingredient }
+      />
+      <span
+        data-testid={ `${index}-card-name` }
+      >
+        { ingredient }
+      </span>
+    </div>
+  );
+}
+
+IngredientsCard.propTypes = ({
+  index: PropTypes.number.isRequired,
+  ingredient: PropTypes.string.isRequired,
+});
+
+export default IngredientsCard;
--- src/pages/FoodExploreIngredients.js
+++ src/pages/FoodExploreIngredients.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import IngredientsCard from '../components/IngredientsCard';
 
 function FoodExploreIngredients() {
+  const [foodIngredients, setfoodIngredients] = useState([]); // variavel que irá armazenar o array com o nome das bebidas após a montagem do componente (conforme useEffect)
+  const MAX_NUMBER = 12;
+
+  const requestFoodIngredients = async () => { // função que faz um fetch para buscar uma lista de ingredientes, retornando um array
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    const request = await fetch(url);
+    const response = await request.json();
+    return response;
+  };
+
+  useEffect(() => {
+    requestFoodIngredients()
+      .then(({ meals }) => setfoodIngredients(meals));
+  });
+
   return (
     <div>
       <Header title="Explorar Ingredientes" search={ false } />
+      {foodIngredients.map((el, i) => {
+        if (i < MAX_NUMBER) {
+          return (
+            <IngredientsCard
+            //  componente que renderiza cada card de comidas (imagem e nome)
+              key={ i }
+              ingredient={ el.strIngredient }
+              index={ i }
+            />
+          );
+        }
+        return '';
+      })}
+      <Footer />
     </div>
   );
 }
