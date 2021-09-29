169d5c1e4f5c7aa537701625bb936156f6985282
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-29 08:40:02 -0300

    Teste em Receitas Feitas e Receitas Favoritas

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import DoneRecipes from '../pages/DoneRecipes';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
 import renderWithRouter from './renderWithRouter';
 
-describe('1 - Verifica os elementos presentes na tela Receitas Feitas', () => {
-  test('Verifica se o título está presente e contém o texto "Receitas Feitas"', () => {
+describe('1 - Verifica os elementos presentes na tela Receitas Favoritas', () => {
+  test('Verifica se o título está presente e contém o texto "Receitas Favoritas"', () => {
     const { getByTestId } = renderWithRouter(
-      <DoneRecipes title="Receitas Feitas" visible={ false } />,
+      <FavoriteRecipes title="Receitas Favoritas" visible={ false } />,
     );
 
     const pageTitle = getByTestId('page-title');
     expect(pageTitle).toBeInTheDocument();
-    expect(pageTitle.innerHTML).toBe('Receitas Feitas');
+    expect(pageTitle.innerHTML).toBe('Receitas Favoritas');
   });
 
   test('Verifica se os botões de filtro estão presentes', () => {
     const { getByTestId } = renderWithRouter(
-      <DoneRecipes title="Receitas Feitas" visible={ false } />,
+      <FavoriteRecipes title="Receitas Favoritas" visible={ false } />,
     );
     const btnAll = getByTestId('filter-by-all-btn');
     const btnFood = getByTestId('filter-by-food-btn');
@@ -27,27 +27,25 @@ describe('1 - Verifica os elementos presentes na tela Receitas Feitas', () => {
   });
 });
 
-describe('2 - Verifica se uma receita feita é carregada', () => {
-  test('Mostra uma receita feita', async () => {
-    const testDoneRecipe = [
+describe('2 - Verifica se uma receita favorita é carregada', () => {
+  test('Mostra uma receita favorita', async () => {
+    const testFavoriteRecipe = [
       {
         alcoholicOrNot: '',
-        doneDate: '29/03/2021',
         area: 'Canadian',
         category: 'Dessert',
         id: '52929',
         image:
-            'https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg',
+          'https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg',
         name: 'Timbits',
         type: 'comida',
-        tags: ['Soup'],
       },
     ];
 
-    localStorage.setItem('doneRecipes', JSON.stringify(testDoneRecipe));
+    localStorage.setItem('favoriteRecipes', JSON.stringify(testFavoriteRecipe));
 
     renderWithRouter(
-      <DoneRecipes title="Receitas Feitas" visible={ false } />,
+      <FavoriteRecipes title="Receitas Favoritas" visible={ false } />,
     );
 
     const recipeName = screen.getByTestId('0-horizontal-name');
